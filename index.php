c28c92e2708b19728149ca0898ed48b037fc0747
durjoydutta <contact.durjoy@gmail.com>
2025-04-03 01:05:56 +0530

    updated task add logic, still got bugs to fix

--- index.php
+++ index.php
@@ -28,30 +28,41 @@
         header("Location: login.php");
     }
     include("database.php");
-
-    $fetch_task_query = "select * from tasks where user_id='{$_SESSION['id']}'";
-    $fetch_task_res = mysqli_query($con, $fetch_task_query);
-    if ($fetch_task_res && mysqli_num_rows($fetch_task_res) > 0) {
-        echo "<br><table border='1'>";
-        echo "<tr><th>Title</th><th>Description</th><th>Status</th><th>Due Date</th><th>Created On</th><th>Set Status</th></tr>";
-        while ($row = mysqli_fetch_assoc($fetch_task_res)) {
-            echo "<tr>";
-            $creationDate = explode(" ", $row["created_at"])[0];
-            echo "<td>{$row["title"]}</td><td>{$row["description"]}</td><td>{$row["status"]}</td><td>{$row["due_date"]}</td><td>{$creationDate}</td>";
-            echo "<td><select name='set-status'>
-                <option value='pending'>Pending</option>
-                <option value='in_progress'>In Progress</option>
-                <option value='complete'>Complete</option>
-            </select></td>";
-            echo "<tr>";
+    function fetch_tasks()
+    {
+        include("database.php");
+        $fetch_task_query = "select * from tasks where user_id='{$_SESSION['id']}'";
+        $fetch_task_res = mysqli_query($con, $fetch_task_query);
+        if ($fetch_task_res && mysqli_num_rows($fetch_task_res) > 0) {
+            echo "<br><table border='1'>";
+            echo "<tr><th>Title</th><th>Description</th><th>Status</th><th>Due Date</th><th>Created On</th><th>Set Status</th></tr>";
+            while ($row = mysqli_fetch_assoc($fetch_task_res)) {
+                echo "<tr>";
+                $creationDate = explode(" ", $row["created_at"])[0];
+                echo "<td>{$row["title"]}</td><td>{$row["description"]}</td><td>{$row["status"]}</td><td>{$row["due_date"]}</td><td>{$creationDate}</td>";
+                echo "<td><select name='set-status'>
+                    <option value='pending'>Pending</option>
+                    <option value='in_progress'>In Progress</option>
+                    <option value='complete'>Complete</option>
+                </select></td>";
+                echo "<tr>";
+            }
+            echo "</table><br>";
         }
-        echo "</table><br>";
+        mysqli_close($con);
     }
+    fetch_tasks();
 
     if (isset($_POST["add-task"])) {
         if (!empty($_POST["new-task"])) {
             $new_task = mysqli_real_escape_string($con, $_POST["new-task"]);
-            $add_task = "insert into tasks(title) values ('{$new_task}')";
+            $add_task_query = "insert into tasks(user_id, title) values ('{$_SESSION["id"]}','{$new_task}')";
+            try {
+                $add_task_res = mysqli_query($con, $add_task_query);
+                // if ($add_task_res) fetch_tasks();
+            } catch (mysqli_sql_exception $e) {
+                echo "Error: " . $e->getMessage();
+            }
         }
     }
 
